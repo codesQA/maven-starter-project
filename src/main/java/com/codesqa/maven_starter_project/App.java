c61092c5a047f33ac64e7d96b56b8967d48b7855
CodesQA <r@hulkar.io>
2018-08-25 22:37:07 +0530

    initial commit

---
+++ src/main/java/com/codesqa/maven_starter_project/App.java
@@ -0,0 +1,13 @@
+package com.codesqa.maven_starter_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
